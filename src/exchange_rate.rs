6ca980015f035d03146418dd18d1b58727ac990d
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2024-03-21 20:39:31 +0100

    Allow inverting exchange rates

--- src/exchange_rate.rs
+++ src/exchange_rate.rs
@@ -19,4 +19,21 @@ where
     pub fn new(rate: f64) -> Self {
         ExchangeRate(rate, PhantomData, PhantomData)
     }
+
+    pub fn invert(&self) -> ExchangeRate<F, T> {
+        ExchangeRate(1.0 / self.0, PhantomData, PhantomData)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::currency::{EUR, USD};
+
+    #[test]
+    fn test_exchange_rate() {
+        let rate = ExchangeRate::<EUR, USD>::new(0.8);
+        assert_eq!(rate, ExchangeRate(0.8, PhantomData, PhantomData));
+        assert_eq!(rate.invert(), ExchangeRate(1.25, PhantomData, PhantomData));
+    }
 }
