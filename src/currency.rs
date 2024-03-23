bd31dcb222c3a5e20adc441af17dcb679c93827e
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2024-03-23 10:45:16 +0100

    Refactor

--- src/currency.rs
+++ src/currency.rs
@@ -1,13 +1,13 @@
-use crate::exchange_rate::ExchangeRate;
 use std::{fmt::Display, marker::PhantomData};
 
 pub trait CurrencyType {
     fn formatter(raw_amount: i64) -> String;
+
+    /// The factor to store the currency amount in the struct.
+    fn store_factor() -> f64;
 }
 
-pub trait CentCurrency: CurrencyType {}
-
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Currency<T>(i64, PhantomData<T>)
 where
     T: CurrencyType;
@@ -22,54 +22,50 @@ impl CurrencyType for EUR {
     fn formatter(raw_amount: i64) -> String {
         format!("€ {:.2}", raw_amount as f64 / 100.0)
     }
+
+    fn store_factor() -> f64 {
+        100.0
+    }
 }
 
 impl CurrencyType for USD {
     fn formatter(raw_amount: i64) -> String {
         format!("$ {:.2}", raw_amount as f64 / 100.0)
     }
+
+    fn store_factor() -> f64 {
+        100.0
+    }
 }
-
-impl CentCurrency for EUR {}
-
-impl CentCurrency for USD {}
 
 impl<T> Currency<T>
 where
     T: CurrencyType,
 {
-    fn new(value: i64) -> Self {
-        Currency(value, PhantomData)
+    pub fn from_raw_amount(raw_amount: i64) -> Self {
+        Currency(raw_amount, PhantomData)
     }
-}
 
-impl<F> Currency<F>
-where
-    F: CentCurrency,
-{
-    pub fn convert<T>(&self, rate: ExchangeRate<T, F>) -> Currency<T>
-    where
-        T: CentCurrency,
-    {
-        Currency::new((self.0 as f64 * rate.0).round() as i64)
+    pub fn raw_amount(&self) -> i64 {
+        self.0
     }
 }
 
 impl<T> From<f64> for Currency<T>
 where
-    T: CentCurrency,
+    T: CurrencyType,
 {
     fn from(value: f64) -> Self {
-        Currency::new((value * 100.0) as i64)
+        Currency::from_raw_amount((value * T::store_factor()) as i64)
     }
 }
 
 impl<T> From<i64> for Currency<T>
 where
-    T: CentCurrency,
+    T: CurrencyType,
 {
     fn from(value: i64) -> Self {
-        Currency::new(value * 100)
+        Currency::from_raw_amount(value * T::store_factor() as i64)
     }
 }
 
@@ -110,14 +106,14 @@ mod tests {
         assert_eq!(format!("{}", amount), "$ 100.00");
     }
 
-    #[test]
-    fn test_currency_convert() {
-        let amount = 100.00;
-        let amount = Currency::<EUR>::from(amount);
+    // #[test]
+    // fn test_currency_convert() {
+    //     let amount = 100.00;
+    //     let amount = Currency::<EUR>::from(amount);
 
-        let rate = ExchangeRate::<USD, EUR>::new(1.2);
-        let us_amount = amount.convert(rate);
+    //     let rate = ExchangeRate::<USD, EUR>::new(1.2);
+    //     let us_amount = amount.convert(rate);
 
-        assert_eq!(format!("{}", us_amount), "$ 120.00");
-    }
+    //     assert_eq!(format!("{}", us_amount), "$ 120.00");
+    // }
 }
--- src/exchange_rate.rs
+++ src/exchange_rate.rs
@@ -1,27 +1,35 @@
-use crate::currency::CurrencyType;
+use crate::currency::{Currency, CurrencyType};
 use std::marker::PhantomData;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct ExchangeRate<T, F>(
-    pub f64,
-    std::marker::PhantomData<T>,
-    std::marker::PhantomData<F>,
+pub struct ExchangeRate<N, D>(
+    f64,
+    std::marker::PhantomData<N>,
+    std::marker::PhantomData<D>,
 )
 where
-    T: CurrencyType,
-    F: CurrencyType;
+    N: CurrencyType,
+    D: CurrencyType;
 
-impl<T, F> ExchangeRate<T, F>
+impl<N, D> ExchangeRate<N, D>
 where
-    T: CurrencyType,
-    F: CurrencyType,
+    N: CurrencyType,
+    D: CurrencyType,
 {
     pub fn new(rate: f64) -> Self {
-        ExchangeRate(rate, PhantomData, PhantomData)
+        ExchangeRate(
+            rate * N::store_factor() / D::store_factor(),
+            PhantomData,
+            PhantomData,
+        )
     }
 
-    pub fn invert(&self) -> ExchangeRate<F, T> {
+    pub fn invert(&self) -> ExchangeRate<D, N> {
         ExchangeRate(1.0 / self.0, PhantomData, PhantomData)
+    }
+
+    pub fn convert(&self, from_amount: Currency<D>) -> Currency<N> {
+        Currency::from_raw_amount((from_amount.raw_amount() as f64 * self.0).round() as i64)
     }
 }
 
@@ -30,10 +38,31 @@ mod tests {
     use super::*;
     use crate::currency::{EUR, USD};
 
+    #[derive(Debug, Eq, PartialEq)]
+    struct USC;
+
+    impl CurrencyType for USC {
+        fn formatter(_: i64) -> String {
+            panic!("Not implemented")
+        }
+
+        fn store_factor() -> f64 {
+            1.0
+        }
+    }
+
     #[test]
     fn test_exchange_rate() {
         let rate = ExchangeRate::<EUR, USD>::new(0.8);
         assert_eq!(rate, ExchangeRate(0.8, PhantomData, PhantomData));
         assert_eq!(rate.invert(), ExchangeRate(1.25, PhantomData, PhantomData));
+    }
+
+    #[test]
+    fn test_convert_different_store_factor() {
+        let rate = ExchangeRate::<USC, USD>::new(100.0);
+        let amount = Currency::<USD>::from(100);
+
+        assert_eq!(rate.convert(amount), Currency::<USC>::from(10000));
     }
 }
