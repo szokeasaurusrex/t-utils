6b6ca8a8829ca6bbe9a36b94e0ca19119f3ae3dc
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2024-05-12 11:48:03 +0200

    `lib` refactor

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+
+use clap::{Parser, Subcommand};
+
+mod commands;
+mod conversions;
+mod io;
+
+/// Simple CLI tool to help with currency conversions.
+#[derive(Debug, Parser)]
+#[command(about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    ConvertTransactions(commands::convert_transactions::ConvertTransactions),
+}
+
+pub fn run_cli() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::ConvertTransactions(args) => {
+            commands::convert_transactions::convert_transactions(&args)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-use std::error::Error;
-
-use clap::{Parser, Subcommand};
-
-mod commands;
-mod conversions;
-mod io;
-
-/// Simple CLI tool to help with currency conversions.
-#[derive(Debug, Parser)]
-#[command(about)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    ConvertTransactions(commands::convert_transactions::ConvertTransactions),
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Commands::ConvertTransactions(args) => {
-            commands::convert_transactions::convert_transactions(&args)
-        }
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    t_utils::run_cli()
 }
